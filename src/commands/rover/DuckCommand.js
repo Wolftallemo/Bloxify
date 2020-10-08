97bc5fd7eea3842a05eb71c7920bf29aaad6210d
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-08 15:45:08 -0400

    Add the duck commands used on my hosted bot

---
+++ src/commands/rover/DuckCommand.js
@@ -0,0 +1,38 @@
+const Command = require('../Command')
+const Discord = require('discord.js')
+const request = require('request-promise')
+const { RichEmbed } = require('discord.js')
+
+module.exports =
+class DuckCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'duck',
+      properName: 'Duck',
+      description: 'Shows random duck',
+      userPermissions: []
+      }
+    )
+  }
+
+  async fn (msg) {
+    let image = 'Unknown'
+    let message = 'Unknown'
+    try {
+      const response = await request({
+        uri: `https://random-d.uk/api/random`,
+        simple: false,
+        resolveWithFullResponse: true
+      })
+    image = JSON.parse(response.body).url
+    message = JSON.parse(response.body).message}
+    catch (e) {
+      return msg.reply(`An error occured! ${e}`)
+    }
+    const embed = new Discord.MessageEmbed()
+    .setTitle(':duck: QUACK! A random duck for you!')
+    .setFooter('Powered by random-d.uk')
+    .setImage(`${image}`)
+    return msg.embed(embed)
+  }
+}
---
+++ src/commands/rover/HttpduckCommand.js
@@ -0,0 +1,38 @@
+const Command = require('../Command')
+const Discord = require('discord.js')
+const request = require('request-promise')
+
+module.exports =
+class HttpduckCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'httpduck',
+      properName: 'HttpDuck',
+      description: 'Shows an http status duck',
+      userPermissions: [],
+      args: [
+        {
+          key: 'httpstatus',
+          label: 'HttpStatus',
+          prompt: 'Status code?',
+          type: 'integer'
+        }
+      ]
+      }
+    )
+  }
+
+  async fn (msg, args) {
+    const code = args.httpstatus
+    const validcodes = ['400','451','302','301','413','404','403','418','420','426','429','200','500','100','409']
+    if (validcodes.includes(`${code}`)) {
+    const embed = new Discord.MessageEmbed()
+    .setFooter('Powered by random-d.uk')
+    .setImage(`https://random-d.uk/api/http/${code}`)
+    return msg.embed(embed)
+    }
+    else {
+      return msg.reply('We do not have a duck with that status code yet.')
+    }
+  }
+}
