28293617cac0498a98f928654c5a6bd73b8b8337
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-09 09:13:35 -0400

    Fix null check again

--- src/commands/rover/BanCommand.js
+++ src/commands/rover/BanCommand.js
@@ -32,7 +32,8 @@ class BanCommand extends Command {
   async fn (msg, args) {
     const rbxuser = args.rbxuser
     const reason = args.reason
-    if (((msg.member.roles.cache.some(roleslist => config.gameModeratorRole.includes(roleslist))) && config.gameModeratorRole != null) || ((modusers.includes(msg.author.id)) && (modusers != null))) {
+    if(config.gameModeratorRole != null) {
+      if ((msg.member.roles.cache.some(roles => config.gameModeratorRole.includes(roles)))) {
         let RBXID = 'Unknown'
         let RBXUSER = 'Unknown'
           try {
@@ -64,6 +65,42 @@ class BanCommand extends Command {
          else {
            return msg.reply('This user does not exist!')
          }
+      }
+    }
+    else if(config.gameModeratorUser != null){
+      if (modusers.includes(msg.author.id)) {
+        let RBXID = 'Unknown'
+        let RBXUSER = 'Unknown'
+          try {
+            const response = await request({
+              uri: `https://api.roblox.com/users/get-by-username?username=${rbxuser}`,
+              simple: false,
+              resolveWithFullResponse: true
+          })
+            RBXID = JSON.parse(response.body).Id
+            RBXUSER = JSON.parse(response.body).Username
+          }
+          catch (e) {
+            return msg.reply(`An error occured! ${e}`)
+          }
+          if (RBXID != undefined) {
+          fs.writeFileSync(`../../data/banfiles/${RBXID}.json`, `{"usercode":"0x2","reason":"${reason}"}`, function (err) {
+            if (err) return msg.reply(err)
+          })
+          const storage = new Storage({keyFilename: config.serviceKeyPath})
+          async function uploadFile() {
+              await storage.bucket(config.bucket).upload(`../../data/banfiles/${RBXID}.json`)
+         }
+         uploadFile()
+         uploadFile().catch(e => {
+           return msg.reply(e.response.statusMessage)
+         })
+         return msg.reply(`${RBXUSER} successfully banned!`)
+        }
+         else {
+           return msg.reply('This user does not exist!')
+         }
+      }
     }
    else {
       return msg.reply('You do not have permission to run this command')
