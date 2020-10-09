94d222d165e49c859a11dca87f4c1f595a1d4ee8
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-09 10:02:55 -0400

    Apparently it hates being wrapped in a function

--- src/commands/rover/BanCommand.js
+++ src/commands/rover/BanCommand.js
@@ -34,20 +34,7 @@ class BanCommand extends Command {
     const reason = args.reason
     if(config.gameModeratorRole.isArray(array) && array.length) {
       if ((msg.member.roles.cache.some(roles => config.gameModeratorRole.includes(roles)))) {
-        makeban()
-      }
-    }
-    else if((config.gameModeratorUsers.isArray(array) && array.length) && (!config.gameModeratorRole.isArray(array) || !array.length)){
-      if (config.gameModeratorUsers.includes(msg.author.id)) {
-        makeban()
-      }
-    }
-   else {
-      return msg.reply('You do not have permission to run this command')
-    }
-  }
-}
-
+        
 async function makeban() {
   let RBXID = 'Unknown'
   let RBXUSER = 'Unknown'
@@ -81,3 +68,48 @@ async function makeban() {
        return msg.reply('This user does not exist!')
      }
   }
+      }
+    }
+    else if((config.gameModeratorUsers.isArray(array) && array.length) && (!config.gameModeratorRole.isArray(array) || !array.length)){
+      if (config.gameModeratorUsers.includes(msg.author.id)) {
+        
+async function makeban() {
+  let RBXID = 'Unknown'
+  let RBXUSER = 'Unknown'
+    try {
+      const response = await request({
+        uri: `https://api.roblox.com/users/get-by-username?username=${rbxuser}`,
+        simple: false,
+        resolveWithFullResponse: true
+      })
+        RBXID = JSON.parse(response.body).Id
+        RBXUSER = JSON.parse(response.body).Username
+      }
+      catch (e) {
+        return msg.reply(`An error occured! ${e}`)
+      }
+      if (RBXID != undefined) {
+      fs.writeFileSync(`${config.banFilesPath}/${RBXID}.json`, `{"usercode":"0x2","reason":"${reason}"}`, function (err) {
+        if (err) return msg.reply(err)
+      })
+      const storage = new Storage({keyFilename: config.serviceKeyPath})
+      async function uploadFile() {
+          await storage.bucket(config.bucket).upload(`${config.banFilesPath}/${RBXID}.json`)
+     }
+     uploadFile()
+     uploadFile().catch(e => {
+       return msg.reply(e.response.statusMessage)
+     })
+     return msg.reply(`${RBXUSER} successfully banned!`)
+    }
+     else {
+       return msg.reply('This user does not exist!')
+     }
+  }
+      }
+    }
+   else {
+      return msg.reply('You do not have permission to run this command')
+    }
+  }
+}
