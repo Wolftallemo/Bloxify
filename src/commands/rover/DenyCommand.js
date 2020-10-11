66050c9ac6be2970bdbbc639b8172eac3bf2d383
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-11 19:09:14 -0400

    Update DenyCommand.js
    can i not read?

--- src/commands/rover/DenyCommand.js
+++ src/commands/rover/DenyCommand.js
@@ -69,7 +69,7 @@ class DenyCommand extends Command {
                                     uri: `https://api.eu.mailgun.net/v3/${config.mailgunDomain}/messages`,
                                     headers: {
                                         'Content-Type': 'multipart/form-data',
-                                        'Authorization': `Basic ${basic_mailgunApiKey}`
+                                        'Authorization': `Basic ${base64_mailgunApiKey}`
                                     },
                                     formData: email
                                 },function (error,response,body) {
