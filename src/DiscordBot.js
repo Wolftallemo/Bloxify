11893220e850255a814dda2dabf6a95a2b61a446
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-11 04:09:19 -0400

    Update DiscordBot.js

--- src/DiscordBot.js
+++ src/DiscordBot.js
@@ -87,6 +87,19 @@ class DiscordBot {
       }
     })
 
+    if (this.isPremium()) {
+      this.bot.dispatcher.addInhibitor(msg => {
+        if (msg.guild && !this.authorizedOwners.includes(msg.guild.ownerID)) {
+          if (this.authorizedOwners.length === 0) {
+            msg.reply('Sorry, the authorized users list is still being downloaded. This occurs when the bot has recently restarted. Please wait a few seconds and try again.')
+          } else {
+            msg.reply(`Sorry, this server isn't authorized to use RoVer Plus.${msg.member.hasPermission(['MANAGE_GUILD']) ? ' The server owner needs to donate at <https://www.patreon.com/erynlynn>, or you can invite the regular RoVer bot at <https://RoVer.link>.' : ''}`) // notify sender to donate only if they're an "admin"
+          }
+          return true
+        }
+      })
+    }
+
     // Register commands
     this.bot.registry
       .registerGroup('rover', 'RoVer')
