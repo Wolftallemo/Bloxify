14f4ae15a7def2778b9e3dfed8576de89f08b9ed
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-11 23:15:48 -0400

    Typo

--- src/commands/rover/AcceptCommand.js
+++ src/commands/rover/AcceptCommand.js
@@ -29,7 +29,7 @@ class AcceptCommand extends Command {
         })
     }
     hasPermission(msg) {
-        msg.members.roles.cache.filter(role => {
+        msg.member.roles.cache.filter(role => {
             return config.appealsManagerRole.includes(role.id)
         })
     }
