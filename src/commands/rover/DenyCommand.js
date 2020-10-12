f6f4d9f98be53b9172c8412fe3f1da428fd1af92
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-12 00:40:10 -0400

    Apply better permission check to deny command

--- src/commands/rover/DenyCommand.js
+++ src/commands/rover/DenyCommand.js
@@ -32,70 +32,65 @@ class DenyCommand extends Command {
         const appealuser = [args.userid]
         const findappeal = `SELECT * FROM appeals WHERE discord_id = $1;`
         if ((config.appealsManagerRole != null) && (config.mailgunApiKey != null) && (config.mailgunDomain != null) && (config.fromAddress != null)){
-            if (msg.member.roles.cache.filter(role => config.appealsManagerRole.includes(role.id))) {
-                client.query(findappeal,appealuser)
-                .then(foundinfo => {
-                    if(foundinfo.rows[0] != null) {
-                        const getemailquery = 'SELECT * FROM auth WHERE discord_id = $1;'
-                        client.query(getemailquery,appealuser)
-                        .then(foundemail => {
-                            const email = {
-                                'from': config.fromAddress,
-                                'to': foundemail.rows[0].email,
-                                'subject': 'Appeal Denied',
-                                'html': `<html>After careful consideration, the moderation team has decided to deny your appeal. We understand that you wish to be unbanned but as of right now we will not accept your appeal. If you have more information that you believe may change this decision, you may send another appeal.<br/><br/>Note from the moderation team: ${args.note}</html>`
-                            }
-                            const mailgunApiKey = Buffer.from(`api:${config.mailgunApiKey}`, 'utf8')
-                            const base64_mailgunApiKey = mailgunApiKey.toString("base64")
-                            if (config.mailgunRegion != 'eu') {
-                                request.post({
-                                    uri: `https://api.mailgun.net/v3/${config.mailgunDomain}/messages`,
-                                    headers: {
-                                        'Content-Type': 'multipart/form-data',
-                                        'Authorization': `Basic ${base64_mailgunApiKey}`
-                                    },
-                                    formData: email
-                                },function (error,response,body) {
-                                    if (response.statusCode == 200) {
-                                        return msg.reply('Appeal denied and user emailed!')
-                                    }
-                                    else {
-                                        return msg.reply(`Mailgun returned an error! (HTTP ${response.statusCode}: ${response.statusMessage})`)
-                                    }
-                                })
-                            }
-                            else {
-                                request.post({
-                                    uri: `https://api.eu.mailgun.net/v3/${config.mailgunDomain}/messages`,
-                                    headers: {
-                                        'Content-Type': 'multipart/form-data',
-                                        'Authorization': `Basic ${base64_mailgunApiKey}`
-                                    },
-                                    formData: email
-                                },function (error,response,body) {
-                                    if (response.statusCode == 200) {
-                                        return msg.reply('Appeal denied and user emailed!')
-                                    }
-                                    else {
-                                        return msg.reply(`Mailgun returned an error! (HTTP ${response.statusCode}: ${response.statusMessage})`)
-                                    }
-                                })
-                            }
-                        })
-                        .catch(e => console.error(e.stack))
-                        const markasresolved = 'DELETE FROM appeals WHERE discord_id = $1;'
-                        client.query(markasresolved,appealuser)
-                        .catch(e => console.error(e.stack))
-                    }
-                    else {
-                        return msg.reply('There are no unresolved appeals under this user account!')
-                    }
-                })
-                .catch(e => console.error(e.stack))
-            }
-            else {
-                return msg.reply('You do not have permission to run this command!')
-            }
+            client.query(findappeal,appealuser)
+            .then(foundinfo => {
+                if(foundinfo.rows[0] != null) {
+                    const getemailquery = 'SELECT * FROM auth WHERE discord_id = $1;'
+                    client.query(getemailquery,appealuser)
+                    .then(foundemail => {
+                        const email = {
+                            'from': config.fromAddress,
+                            'to': foundemail.rows[0].email,
+                            'subject': 'Appeal Denied',
+                            'html': `<html>After careful consideration, the moderation team has decided to deny your appeal. We understand that you wish to be unbanned but as of right now we will not accept your appeal. If you have more information that you believe may change this decision, you may send another appeal.<br/><br/>Note from the moderation team: ${args.note}</html>`
+                        }
+                        const mailgunApiKey = Buffer.from(`api:${config.mailgunApiKey}`, 'utf8')
+                        const base64_mailgunApiKey = mailgunApiKey.toString("base64")
+                        if (config.mailgunRegion != 'eu') {
+                            request.post({
+                                uri: `https://api.mailgun.net/v3/${config.mailgunDomain}/messages`,
+                                headers: {
+                                    'Content-Type': 'multipart/form-data',
+                                    'Authorization': `Basic ${base64_mailgunApiKey}`
+                                },
+                                formData: email
+                            },function (error,response,body) {
+                                if (response.statusCode == 200) {
+                                    return msg.reply('Appeal denied and user emailed!')
+                                }
+                                else {
+                                    return msg.reply(`Mailgun returned an error! (HTTP ${response.statusCode}: ${response.statusMessage})`)
+                                }
+                            })
+                        }
+                        else {
+                            request.post({
+                                uri: `https://api.eu.mailgun.net/v3/${config.mailgunDomain}/messages`,
+                                headers: {
+                                    'Content-Type': 'multipart/form-data',
+                                    'Authorization': `Basic ${base64_mailgunApiKey}`
+                                },
+                                formData: email
+                            },function (error,response,body) {
+                                if (response.statusCode == 200) {
+                                    return msg.reply('Appeal denied and user emailed!')
+                                }
+                                else {
+                                    return msg.reply(`Mailgun returned an error! (HTTP ${response.statusCode}: ${response.statusMessage})`)
+                                }
+                            })
+                        }
+                    })
+                    .catch(e => console.error(e.stack))
+                    const markasresolved = 'DELETE FROM appeals WHERE discord_id = $1;'
+                    client.query(markasresolved,appealuser)
+                    .catch(e => console.error(e.stack))
+                }
+                else {
+                    return msg.reply('There are no unresolved appeals under this user account!')
+                }
+            })
+            .catch(e => console.error(e.stack))
         }
         else {
             return msg.reply('Make sure your appeal manager roles and mailgun information are set!')
