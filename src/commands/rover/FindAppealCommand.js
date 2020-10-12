9b54fc74007dd1d46fef1527cffafe70053c64c4
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-12 00:55:05 -0400

    Apply new permission checks to these too

--- src/commands/rover/FindAppealCommand.js
+++ src/commands/rover/FindAppealCommand.js
@@ -21,30 +21,30 @@ class FindAppealCommand extends Command {
             ]
         })
     }
-
+    hasPermission(msg){
+        return msg.member.roles.cache.some(role => config.appealsManagerRole.includes(role.id))
+    }
     async fn(msg, args) {
         const query = 'SELECT appeals.discord_id, auth.username, auth.discriminator, appeals.reason, appeals.comment, appeals.date FROM appeals,auth where appeals.discord_id = $1 AND auth.discord_id = $1;'
         const val = [args.userid]
-        if (msg.member.roles.cache.filter(role => config.appealsManagerRole.includes(role.id))) {
-            client.query(query,val)
-            .then(found => {
-                if (found.rows[0] != null) {
-                    const embed = new Discord.MessageEmbed()
-                    .setTitle(`Appeal for ${found.rows[0].username}#${('0000'+found.rows[0].discriminator).slice(-4)} (${list.rows[0].discord_id})`)
-                    .addFields(
-                        {name: 'Reason for ban', value: `${found.rows[0].reason}`},
-                        {name: 'Comment', value: `${found.rows[0].comment}`},
-                        {name: 'Time', value:`${found.rows[0].date}`}
-                    )
-                    .setColor(3756250)
-                    .setTimestamp()
-                    return msg.embed(embed)
-                }
-                else {
-                    return msg.reply('There are no unresolved appeals under this user account!')
-                }
-            })
-            .catch(e => console.error(e.stack))
-        }
+        client.query(query,val)
+        .then(found => {
+            if (found.rows[0] != null) {
+                const embed = new Discord.MessageEmbed()
+                .setTitle(`Appeal for ${found.rows[0].username}#${('0000'+found.rows[0].discriminator).slice(-4)} (${list.rows[0].discord_id})`)
+                .addFields(
+                    {name: 'Reason for ban', value: `${found.rows[0].reason}`},
+                    {name: 'Comment', value: `${found.rows[0].comment}`},
+                    {name: 'Time', value:`${found.rows[0].date}`}
+                )
+                .setColor(3756250)
+                .setTimestamp()
+                return msg.embed(embed)
+            }
+            else {
+                return msg.reply('There are no unresolved appeals under this user account!')
+            }
+        })
+        .catch(e => console.error(e.stack))
     }
 }
--- src/commands/rover/GetAppealsCommand.js
+++ src/commands/rover/GetAppealsCommand.js
@@ -13,31 +13,29 @@ class GetAppealsCommand extends Command {
             userPermissions: []
         })
     }
+    hasPermission(msg){
+        return msg.member.roles.cache.some(role => config.appealsManagerRole.includes(role.id))
+    }
 
     async fn(msg) {
-        if (msg.member.roles.cache.filter(role => config.appealsManagerRole.includes(role.id))) {
-            client.query('SELECT appeals.discord_id, auth.username, auth.discriminator FROM appeals,auth WHERE appeals.discord_id = auth.discord_id;')
-            .then(list => {
-                if (list.rows[0] == null) {
-                    return msg.reply('No open appeals were found!')
+        client.query('SELECT appeals.discord_id, auth.username, auth.discriminator FROM appeals,auth WHERE appeals.discord_id = auth.discord_id;')
+        .then(list => {
+            if (list.rows[0] == null) {
+                return msg.reply('No open appeals were found!')
+            }
+            else {
+                var users = ''
+                for(var i = 0;i < list.rowCount;i++) {
+                    users = `${users}\n\n${list.rows[i].username}#${('0000'+list.rows[i].discriminator).slice(-4)} (${list.rows[i].discord_id})`
                 }
-                else {
-                    var users = ''
-                    for(var i = 0;i < list.rowCount;i++) {
-                        users = `${users}\n\n${list.rows[i].username}#${('0000'+list.rows[i].discriminator).slice(-4)} (${list.rows[i].discord_id})`
-                    }
-                    const embed = new Discord.MessageEmbed()
-                    .setTitle('Open Appeals')
-                    .setDescription(`${users}`)
-                    .setColor(3756250)
-                    .setTimestamp()
-                    return msg.embed(embed)
-                }
-            })
-            .catch(e => console.error(e.stack))
-        }
-        else {
-            return msg.reply('You do not have permission to run this command!')
-        }
+                const embed = new Discord.MessageEmbed()
+                .setTitle('Open Appeals')
+                .setDescription(`${users}`)
+                .setColor(3756250)
+                .setTimestamp()
+                return msg.embed(embed)
+            }
+        })
+        .catch(e => console.error(e.stack))
     }
 }
