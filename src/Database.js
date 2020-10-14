63f0997a10f4be511ac1356622176670ecd140fb
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-14 01:41:41 -0400

    Actually set defaults if no setting provided

--- src/Database.js
+++ src/Database.js
@@ -1,12 +1,27 @@
 const { Client } = require('pg')
 const dbconfig = require('./data/client.json')
+function getDbUser() {
+    if (!dbconfig.databaseUser) {
+        return 'postgres'
+    }
+    return dbconfig.databaseUser
+}
+
+function getDbName() {
+    if (!dbconfig.databaseName) {
+        return 'postgres'
+    }
+    return dbconfig.databaseName
+}
+const dbUser = getDbUser()
+const dbName = getDbName()
 const client = new Client({
-    user: dbconfig.databaseUser,
+    user: dbUser,
     host: dbconfig.databaseAddress,
-    database: dbconfig.databaseName,
+    database: dbName,
     password: dbconfig.databasePassword,
     port: 5432,
 })
-if ((dbconfig.databaseUser) && (dbconfig.databaseAddress) && (dbconfig.databaseName) && (dbconfig.databasePassword)) client.connect()
+if ((dbconfig.databaseAddress) && (dbconfig.databasePassword)) client.connect()
 
 module.exports = client
