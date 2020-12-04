ca28044a9b1b85422f10d8c7e6e8ce333475a1c5
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-03 19:53:29 -0500

    Make getlatest command

---
+++ src/commands/rover/GetLatestCommand.js
@@ -0,0 +1,29 @@
+const Command = require('../Command')
+const config = require('../../data/client.json')
+const { exec } = require("child_process")
+
+module.exports =
+class GetLatestCommand extends Command {
+    constructor (client) {
+        super(client, {
+            name: 'getlatest',
+            properName: 'GetLatest',
+            description: 'Retrieves latest version of code from repo.',
+            userPermissions: []
+        })
+    }
+    hasPermission(msg) {
+        return msg.member.id === config.owner
+    }
+    async fn (msg) {
+        exec('git pull', function(error, stdout, stderr) {
+            if (error) {
+                console.error(error)
+                return msg.reply(error)
+            }
+            await msg.reply(stdout)
+            await msg.channel.send('Restarting...')
+            process.exit()
+        })
+    }
+}
