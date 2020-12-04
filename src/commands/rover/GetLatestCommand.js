8885a6e8b21ab940617b08bad9370f6fb312ec04
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-04 11:03:36 -0500

    Destroy session on getlatest

--- src/commands/rover/GetLatestCommand.js
+++ src/commands/rover/GetLatestCommand.js
@@ -1,5 +1,6 @@
 const Command = require('../Command')
 const config = require('../../data/client.json')
+const DiscordBot = require('../../DiscordBot')
 const { exec } = require("child_process")
 
 module.exports =
@@ -24,6 +25,7 @@ class GetLatestCommand extends Command {
             await msg.reply(stdout)
             if (stdout.match((/Already up to date\./i))) return
             await msg.channel.send('Restarting...')
+            await DiscordBot.bot.destroy()
             process.exit()
         })
     }
